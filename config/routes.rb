212103c356d363ba35231c9ed5f7b473f0fb99db
Jose Cerrato <jfcerrato92@gmail.com>
2024-05-21 22:51:00 -0600

    Yelp MVP #2 - New with valid parameters - COMPLETED

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'restaurants', to: 'restaurants#index'
   get 'restaurants/new', to: 'restaurants#new'
   post 'restaurants', to: 'restaurants#create'
-  get 'restaurants/:id', to: 'restaurants#show'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
