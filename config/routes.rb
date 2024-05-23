a42e88a64738cb4d69474f379d78eba4aded3b51
Jose Cerrato <jfcerrato92@gmail.com>
2024-05-23 17:05:17 -0600

    Challenge #2 - RailsYelpMvp - COMPLETED

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'restaurants/new', to: 'restaurants#new'
   post 'restaurants', to: 'restaurants#create'
   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
+  # resources :reviews, only: %i[new create]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
